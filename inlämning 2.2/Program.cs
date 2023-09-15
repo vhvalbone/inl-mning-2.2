4290c68d723a796fe32fda6f54b59562fe0c6406
alsv06003 <alsv06003@E-PW0234JW>
2023-09-15 12:49:45 +0200

    Add project files.

---
+++ inlämning 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+    namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur långt hoppade du Elin mått i meter");
+            string långtText = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade du Alma mått i meter");
+            string hoppadeText = Console.ReadLine();
+            double långt = double.Parse(långtText);
+            double hoppade = double.Parse(hoppadeText);
+            double skillnad = långt - hoppade;
+            Console.WriteLine("elin hoppade" + skillnad + "längre än alma");
+        }
+    }
+}
